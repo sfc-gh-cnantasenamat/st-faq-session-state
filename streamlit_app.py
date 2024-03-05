b330c101a311b7fae6322e2048833055b4e33040
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-03-05 10:59:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,17 +20,29 @@ st.header('Session state')
 
 # Initialize session state variables
 if 'count' not in st.session_state:
-	st.session_state.count = 0
+   st.session_state.count = 0
 
 # Update session state variables
 if st.button('Increment'):
-    st.session_state.count += 1
+   st.session_state.count += 1
 
 if st.button('Decrement'):
-    st.session_state.count -= 1
+   st.session_state.count -= 1
 
 # Print session state variable
-st.write('Count = ', st.session_state.count)
+st.write('Count: ', st.session_state.count)
 
 #############################################
 # 2.2 Callback functions
+st.header('Callback functions')
+
+
+if 'squared' not in st.session_state:
+   st.session_state.squared = 0
+
+def callback():
+   st.session_state.squared = x * 2
+
+x = st.slider('Selct a value for **x**:', 1, 9, 1, on_change=callback)
+
+st.write('Squared: ', st.session_state.squared)
