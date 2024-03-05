e9796d204995d2898b89a90ea881903e86aaa9aa
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-03-05 11:10:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,16 +42,19 @@ st.header('Callback functions')
 
 st.info('A counter example using session state and callback functions.')
 
+# Initialize session state variables
 if 'count_value' not in st.session_state:
     st.session_state.count_value = 0
 
+# Callback functions
 def increment_counter():
     st.session_state.count_value += 1
    
 def decrement_counter():
     st.session_state.count_value -= 1
-   
+
 st.button('Increment', on_click=increment_counter, key='increment_btn')
 st.button('Decrement', on_click=decrement_counter, key='decrement_btn')
 
+# Print session state variable
 st.write('Count = ', st.session_state.count_value)
