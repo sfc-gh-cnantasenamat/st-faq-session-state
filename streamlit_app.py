3413a416bb6ada8c55c51de5c893822e77d115d9
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-03-05 10:48:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,24 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('🎈 FAQ: How to prevent app reruns')
 
-st.write('Hello world!')
+#############################################
+# 2.1 Session state
+st.header('Session state')
+
+# Initialize session state variables
+if 'count' not in st.session_state:
+	st.session_state.count = 0
+
+# Update session state variables
+if st.button('Increment'):
+    st.session_state.count += 1
+
+if st.button('Decrement'):
+    st.session_state.count -= 1
+
+# Print session state variable
+st.write('Count = ', st.session_state.count)
+
+#############################################
+
